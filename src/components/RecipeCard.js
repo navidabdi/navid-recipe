82c92c4cbd0c31e9f141cf3431f7c6ba219f96ec
navidabdi <artsanaat@gmail.com>
2022-09-03 09:36:12 +0300

    add swiper

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,57 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
 const RecipeCard = ({ recipes }) => {
   return (
-    <div>
-      {/* {console.log(recipes)} */}
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={10}
+      slidesPerView={2}
+      navigation
+      breakpoints={{
+        // when window width is >= 640px
+        640: {
+          width: 640,
+          slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        768: {
+          width: 768,
+          slidesPerView: 3,
+        },
+        // when window width is >= 992px
+        992: {
+          width: 992,
+          slidesPerView: 5,
+        },
+      }}
+    >
       {recipes.length > 0
         ? recipes.map((recipe) => (
-            <article key={recipe.id}>
-              <img src={recipe.image} alt="" />
-            </article>
+            <SwiperSlide>
+              <article
+                key={recipe.id}
+                className="rounded-xl overflow-hidden bg-white"
+              >
+                <img
+                  className="w-full h-[120px] object-cover"
+                  src={recipe.image}
+                  alt=""
+                />
+                <div className="py-3 px-2">
+                  <h3 className="text-[10px] truncate">{recipe.title}</h3>
+                </div>
+              </article>
+            </SwiperSlide>
           ))
         : ""}
-    </div>
+    </Swiper>
   );
 };
 
